9a17a1e980e8c0de9220e0cd1db0156024b5f04b
Enrico Piazza <enry2077@gmail.com>
2015-11-20 17:52:19 +0000

    modified, no robot pose necessary

--- src/acquirer.py
+++ src/acquirer.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
 	tf_broadcaster = tf.TransformBroadcaster()
 	
 	# Robot position and yaw
-	robot_pose = rospy.get_param('robot_pose')
+#	robot_pose = rospy.get_param('robot_pose')
 	
 	# Acquire the marker-robot transform
 	transform_captured = False
@@ -25,21 +25,22 @@ if __name__ == '__main__':
 			
 			rospy.loginfo("acquiring transform from mocap")
 			
-			now = rospy.Time.now()
+		#	now = rospy.Time.now()
 			
 			# Robot pose
-			(x_r0, y_r0, theta_r0) = (robot_pose['x'], robot_pose['y'], robot_pose['theta'])
-			robot_rotation = tf.transformations.quaternion_from_euler(0, 0, theta_r0)
+		#	(x_r0, y_r0, theta_r0) = (robot_pose['x'], robot_pose['y'], robot_pose['theta'])
+		#	robot_rotation = tf.transformations.quaternion_from_euler(0, 0, theta_r0)
 			
 			# broadcast the robot frame relative to the marker
-			tf_broadcaster.sendTransform((x_r0, y_r0, 0.0), robot_rotation, now, "/actual_robot", "/world")
+		#	tf_broadcaster.sendTransform((x_r0, y_r0, 0.0), robot_rotation, now, "/actual_robot", "/world")
 			
 			# receive the robot transform and compute the robot pose
-			tf_listener.waitForTransform("/world", "/actual_robot", now, rospy.Duration(5.0))
-			marker_to_robot_transform = tf_listener.lookupTransform("/robot_marketset", "/actual_robot", now)
+		#	tf_listener.waitForTransform("/world", "/actual_robot", now, rospy.Duration(5.0))
+			marker_to_robot_transform = tf_listener.lookupTransform("/robot_marketset", "/world", rospy.Time(0))
 			
 			transform_captured = True
 			
+			print tf_listener.lookupTransform("/robot_marketset", "/world", rospy.Time(0))
 			
 			( (mrt_x, mrt_y, _), mrt_rotation ) = marker_to_robot_transform
 			(_, _, mrt_theta) = tf.transformations.euler_from_quaternion(mrt_rotation)
@@ -59,7 +60,7 @@ if __name__ == '__main__':
 			rospy.set_param('marker_to_robot_transform_2D', mrt_pose)
 			rospy.set_param('marker_to_robot_transform', marker_to_robot_transform)
 						
-			rospy.set_param('info', "marker_to_robot_transform is the tf transform from the markerset frame to the robot frame. The robot frame is computed from robot_pose")
+#			rospy.set_param('info', "marker_to_robot_transform is the tf transform from the markerset frame to the robot frame. The robot frame is computed from robot_pose")
 			
 			os.system("rosparam dump " + filename + " " + rospy.get_namespace())
 			
