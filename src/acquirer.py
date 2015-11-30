a4d9395f2676fff93ff0d2af776a5bd941ff924e
Enrico Piazza <enry2077@gmail.com>
2015-11-30 16:20:39 +0000

    simplified everything

--- src/acquirer.py
+++ src/acquirer.py
@@ -36,32 +36,38 @@ if __name__ == '__main__':
 			
 			# receive the robot transform and compute the robot pose
 		#	tf_listener.waitForTransform("/world", "/actual_robot", now, rospy.Duration(5.0))
+			
+			
 			marker_to_robot_transform = tf_listener.lookupTransform("/robot_markerset", "/world", rospy.Time(0))
+			
+			robot_to_marker_transform = tf_listener.lookupTransform("/world", "/robot_markerset", rospy.Time(0))
+			
+			print "/robot_markerset to /world", marker_to_robot_transform
+			
+			print "/world to /robot_markerset", robot_to_marker_transform
 			
 			transform_captured = True
 			
-			print "/robot_markerset to /world", tf_listener.lookupTransform("/robot_markerset", "/world", rospy.Time(0))
+		#	( (mrt_x, mrt_y, _), mrt_rotation ) = marker_to_robot_transform
+		#	(_, _, mrt_theta) = tf.transformations.euler_from_quaternion(mrt_rotation)
 			
-			print "/world to /robot_markerset", tf_listener.lookupTransform("/world", "/robot_markerset", rospy.Time(0))
-			
-			( (mrt_x, mrt_y, _), mrt_rotation ) = marker_to_robot_transform
-			(_, _, mrt_theta) = tf.transformations.euler_from_quaternion(mrt_rotation)
-	
-			mrt_pose = (mrt_x, mrt_y, mrt_theta)
+		#	mrt_pose = (mrt_x, mrt_y, mrt_theta)
 			
 		#	rospy.loginfo("marker-robot transform acquired")
 		#	rospy.loginfo("transform: \t"+str(marker_to_robot_transform))
 		#	rospy.loginfo("to 2D: \t"+str(mrt_pose))
 			
-			robot_info = raw_input("insert a string (without spaces) that identify the robot, es: the name of the team\t")
+			robot_info = raw_input("insert the name of the team (wothout spaces): ")
 			
-			filename = "~/logs/" + rospy.get_namespace().strip("/") + "-" + robot_info.replace(" ", "") + "-" + strftime("%Y-%m-%d_%H:%M:%S", gmtime()) + ".yaml"
+		#	filename = "~/logs/" + rospy.get_namespace().strip("/") + "-" + robot_info.replace(" ", "") + "-" + strftime("%Y-%m-%d_%H:%M:%S", gmtime()) + ".yaml"
+			filename = "~/logs/transform-" + robot_info.replace(" ", "") + "-" + strftime("%Y-%m-%d_%H:%M:%S", gmtime()) + ".yaml"
 			
 			rospy.set_param('robot_info', robot_info)
 			
 		#	rospy.set_param('marker_to_robot_transform_2D', mrt_pose)
+			rospy.set_param('robot_to_marker_transform', robot_to_marker_transform)
 			rospy.set_param('marker_to_robot_transform', marker_to_robot_transform)
-						
+				
 #			rospy.set_param('info', "marker_to_robot_transform is the tf transform from the markerset frame to the robot frame. The robot frame is computed from robot_pose")
 			
 			os.system("rosparam dump " + filename + " " + rospy.get_namespace())
